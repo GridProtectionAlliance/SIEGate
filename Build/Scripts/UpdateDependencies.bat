1a00f78d574c6ba4275cc2e23a06d2993990d24e
StephenCWills <swills@gridprotectionalliance.org>
2013-06-25 19:23:07 +0000

    Feature Development: Removed commands to update openPDC dependencies in UpdateDependencies.bat.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/siegate/Feature Development;C85553

--- Build/Scripts/UpdateDependencies.bat
+++ Build/Scripts/UpdateDependencies.bat
@@ -19,8 +19,6 @@ SET vs="%VS110COMNTOOLS%\..\IDE\devenv.com"
 SET tfs="%VS110COMNTOOLS%\..\IDE\tf.exe"
 SET source1="\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Libraries\*.*"
 SET target1="..\..\Source\Dependencies\GSF"
-SET source2="\\GPAWEB\NightlyBuilds\openPDC\Beta-VS2012\Libraries\*.*"
-SET target2="..\..\Source\Dependencies\openPDC"
 SET solution="..\..\Source\SIEGate.sln"
 SET sourcetools=..\..\Source\Applications\SIEGate\SIEGateSetup\
 SET frameworktools=\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Tools\
@@ -29,7 +27,6 @@ SET /p checkin=Check-in updates (Y or N)?
 ECHO.
 ECHO Getting latest version...
 %tfs% get %target1% /version:T /force /recursive /noprompt
-%tfs% get %target2% /version:T /force /recursive /noprompt
 %tfs% get "%sourcetools%ConfigCrypter.exe" /version:T /force /recursive /noprompt
 %tfs% get "%sourcetools%ConfigurationEditor.exe" /version:T /force /recursive /noprompt
 %tfs% get "%sourcetools%DataMigrationUtility.exe" /version:T /force /recursive /noprompt
@@ -47,7 +44,6 @@ ECHO Checking out dependencies...
 
 ECHO.
 ECHO Updating dependencies...
-XCOPY %source2% %target2% /Y /U
 XCOPY %source1% %target1% /Y /U
 XCOPY "%frameworktools%ConfigCrypter\ConfigCrypter.exe" "%sourcetools%ConfigCrypter.exe" /Y
 XCOPY "%frameworktools%ConfigEditor\ConfigEditor.exe" "%sourcetools%ConfigurationEditor.exe" /Y
@@ -66,7 +62,6 @@ GOTO Finalize
 ECHO.
 ECHO Checking in dependencies...
 %tfs% checkin %target1% /noprompt /recursive /comment:"VS2012: Updated grid solutions framework dependencies."
-%tfs% checkin %target2% /noprompt /recursive /comment:"VS2012: Updated openPDC dependencies."
 %tfs% checkin "%sourcetools%ConfigCrypter.exe" /noprompt /comment:"VS2012: Updated grid solutions framework tool: ConfigCrypter."
 %tfs% checkin "%sourcetools%ConfigurationEditor.exe" /noprompt /comment:"VS2012: Updated grid solutions framework tool: ConfigurationEditor."
 %tfs% checkin "%sourcetools%DataMigrationUtility.exe" /noprompt /comment:"VS2012: Updated grid solutions framework tool: DataMigrationUtility."
