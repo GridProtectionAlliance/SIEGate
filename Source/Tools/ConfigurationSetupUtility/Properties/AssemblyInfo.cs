419ab0d4bdcc1b758ff6d9b2668f54e57f014598
gsfbuildbot <support@gridprotectionalliance.org>
2015-03-29 04:23:16 +0000

    SIEGate: Version change for build SIEGate_v1.1.101.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/siegate/Feature Development;C94975

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.100.0")]
-[assembly: AssemblyVersion("1.1.100.0")]
+// [assembly: AssemblyVersion("1.1.101.0")]
+[assembly: AssemblyVersion("1.1.101.0")]
