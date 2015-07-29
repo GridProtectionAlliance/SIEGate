bfc5ef06a7ee0f95273891e2bc4cd9177c072a42
gsfbuildbot <support@gridprotectionalliance.org>
2015-07-29 07:05:38 +0000

    SIEGate: Version change for build SIEGate_v1.1.145.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/siegate/Feature Development;C97208

--- Source/Applications/SIEGate/SIEGate/Properties/AssemblyInfo.cs
+++ Source/Applications/SIEGate/SIEGate/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c8a353e9-9383-46c5-8c19-8da53d307cde")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.144.0")]
-[assembly: AssemblyFileVersion("1.1.144.0")]
+[assembly: AssemblyVersion("1.1.145.0")]
+[assembly: AssemblyFileVersion("1.1.145.0")]
