50cca35e79ff7b1d7c33da5bc9fc356fdca913b7
gsfbuildbot <support@gridprotectionalliance.org>
2019-02-04 00:24:42 -0500

    SIEGate: Version change for build v1.6.116.0-master.

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.115.0")]
-[assembly: AssemblyVersion("1.6.115.0")]
+// [assembly: AssemblyVersion("1.6.116.0")]
+[assembly: AssemblyVersion("1.6.116.0")]
