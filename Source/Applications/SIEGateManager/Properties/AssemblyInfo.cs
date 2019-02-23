3c9e2738685b0befdf2a253a952099aacb5ac3fc
gsfbuildbot <support@gridprotectionalliance.org>
2019-02-23 00:24:45 -0500

    SIEGate: Version change for build v1.6.130.0-master.

--- Source/Applications/SIEGateManager/Properties/AssemblyInfo.cs
+++ Source/Applications/SIEGateManager/Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.129.0")]
-[assembly: AssemblyVersion("1.6.129.0")]
-[assembly: AssemblyFileVersion("1.6.129.0")]
+// [assembly: AssemblyVersion("1.6.130.0")]
+[assembly: AssemblyVersion("1.6.130.0")]
+[assembly: AssemblyFileVersion("1.6.130.0")]
