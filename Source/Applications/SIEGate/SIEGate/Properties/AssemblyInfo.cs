f7ed2629362990d172b634c70a791502a09e1ca7
gsfbuildbot <support@gridprotectionalliance.org>
2020-03-27 00:46:59 -0400

    SIEGate: Version change for build v1.7.114.0-master.

--- Source/Applications/SIEGate/SIEGate/Properties/AssemblyInfo.cs
+++ Source/Applications/SIEGate/SIEGate/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c8a353e9-9383-46c5-8c19-8da53d307cde")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.7.113.0")]
-[assembly: AssemblyFileVersion("1.7.113.0")]
+[assembly: AssemblyVersion("1.7.114.0")]
+[assembly: AssemblyFileVersion("1.7.114.0")]
