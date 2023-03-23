3d9f3e15807ef16b4b6a1a22359cec78a53ca360
gsfbuildbot <support@gridprotectionalliance.org>
2023-03-23 01:20:58 -0400

    SIEGate: Version change for build v1.8.232.0-master.

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.8.231.0")]
-[assembly: AssemblyVersion("1.8.231.0")]
+// [assembly: AssemblyVersion("1.8.232.0")]
+[assembly: AssemblyVersion("1.8.232.0")]
