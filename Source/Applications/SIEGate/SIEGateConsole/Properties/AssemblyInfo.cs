057afeb73c1693941d81cc008da8673f60472da2
gsfbuildbot <support@gridprotectionalliance.org>
2026-04-17 00:56:23 -0400

    SIEGate: Version change for build v1.8.450.0-master.

--- Source/Applications/SIEGate/SIEGateConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/SIEGate/SIEGateConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.8.449.0")]
+[assembly: AssemblyVersion("1.8.450.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
